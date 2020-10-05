0d49f224e2176515a05efb6ee6cc49fa9eafbbdd
Jon Jessee <tjessee7624@gmail.com>
2020-10-05 17:46:14 -0600

    Created POST request and began work on the DELETE request. POST was successfully tested.

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -17,9 +17,9 @@ app.get('/notes', (req, res) => {
 });
 
 // * returns the index.html page
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
 
 // GET /api/notes reads the db.json file and retuns all saved notes
 app.get('/api/notes', (req, res) => {
@@ -30,10 +30,37 @@ app.get('/api/notes', (req, res) => {
 
 
 //POST /api/notes - Receive a new note on the req.body and add to db.json, return new note to client
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    console.log(newNote);
+    // res.json(newNote);
+
+
+
+    fs.readFile('../db/db.json', 'utf8', (err, data) => {
+        if(err) {
+            throw err
+        }
+        let json = JSON.parse(data);
+        json.push(newNote);
+
+        fs.writeFile('../db/db.json', JSON.stringify(json), (err) => {
+            if (err) {
+                throw err
+            };
+        });
+        console.log('File successfully updated!');
+    });
+});
 
 //DELETE /api/notes/:id
 //Receive query parameter to locate note to delete. In order to delete a note, read all the notes from db.json
-//remove the note with th egiven id and rewrite notes to db.json
+//remove the note with the given id and rewrite notes to db.json
+app.delete('/api/notes:title', (req, res) => {
+    let selected = req.params.title;
+    console.log(selected);
+    console.log(`${selected} has been deleted!`);
+});
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
