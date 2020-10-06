d51cf706f1b031c4355c748b73e7dd642ceaf3d4
Jon Jessee <tjessee7624@gmail.com>
2020-10-06 11:56:40 -0600

    Restructuring POST and DELETE requests. Removed util module and promisified readFile function.

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -1,66 +1,83 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
 const app = express();
 const PORT = 3000;
-const readFileAsync = util.promisify(fs.readFile);
+
 
 //Middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-//GET /notes will return the notes.html page
+//GET requests
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
-// * returns the index.html page
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
-// GET /api/notes reads the db.json file and retuns all saved notes
 app.get('/api/notes', (req, res) => {
-    readFileAsync('../db/db.json', 'utf8').then(data => {
-        return res.json(data);
-    });
-});
-
-
-//POST /api/notes - Receive a new note on the req.body and add to db.json, return new note to client
-app.post('/api/notes', (req, res) => {
-    let newNote = req.body;
-    console.log(newNote);
-    // res.json(newNote);
-
-
-
     fs.readFile('../db/db.json', 'utf8', (err, data) => {
         if(err) {
             throw err
         }
-        let json = JSON.parse(data);
-        json.push(newNote);
-
-        fs.writeFile('../db/db.json', JSON.stringify(json), (err) => {
-            if (err) {
-                throw err
-            };
-        });
-        console.log('File successfully updated!');
+        return res.json(data);
     });
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+//POST request
+app.post('/api/notes', (req, res) => {
+    let newNote = req.body;
+    
+});
+
+//POST /api/notes - Receive a new note on the req.body and add to db.json, return new note to client
+// app.post('/api/notes', (req, res) => {
+//     let newNote = req.body;
+//     console.log(newNote);
+    
+
+
+
+//     fs.readFile('../db/db.json', 'utf8', (err, data) => {
+//         if(err) {
+//             throw err
+//         }
+//         let json = JSON.parse(data);
+//         json.push(newNote);
+
+//         fs.writeFile('../db/db.json', JSON.stringify(json), (err) => {
+//             if (err) {
+//                 throw err
+//             };
+//         });
+//         console.log('File successfully updated!');
+//     });
+// });
 
 //DELETE /api/notes/:id
 //Receive query parameter to locate note to delete. In order to delete a note, read all the notes from db.json
 //remove the note with the given id and rewrite notes to db.json
-app.delete('/api/notes:title', (req, res) => {
-    let selected = req.params.title;
-    console.log(selected);
-    console.log(`${selected} has been deleted!`);
-});
+// app.delete('/api/notes:title', (req, res) => {
+//     let selected = req.params.title;
+//     fs.readFile('../db/db.json', 'utf8', (err, data) => {
+//         if(err) {
+//             throw err
+//         }
+
+//         let json = JSON.parse(data);
+//         const toDelete = json.find(note => note.title === selected);
+//         if(!toDelete) {
+//             res.status(404).send('Note with given title could not be located');
+//         };
+//         const index = json.indexOf(toDelete);
+//         json.splice(index, 1);
+//     });   
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
