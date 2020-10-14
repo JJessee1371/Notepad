7261631db045bf8ff27e080a9a8e966c638e359b
Jon Jessee <tjessee7624@gmail.com>
2020-10-14 15:09:25 -0600

    Completed functionality for all routes.

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use( express.static( __dirname + '/public' ));
+app.use(express.static( __dirname + '/public' ));
 //Middleware for data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
 
 
 //GET requests
@@ -17,11 +21,12 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    fs.readFile('./db/db.json', (err, data) => {
         if(err) {
             throw err
         }
-        return res.json(data);
+        
+        return res.json(JSON.parse(data.toString()));
     });
 });
 
@@ -32,47 +37,52 @@ app.get('*', (req, res) => {
 
 //POST request
 app.post('/api/notes', (req, res) => {
-    let newNote = req.body;
+    let note = req.body;
+    note["id"] = Date.now();
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if(err) {
             throw err
         }
         let json = JSON.parse(data);
-        json.push(newNote);
+        json.push(note);
 
         fs.writeFile('./db/db.json', JSON.stringify(json), (err) => {
             if (err) {
                 throw err
-            };
+            }
+            res.json(note);
         });
+
     });
 });
 
 
 //DELETE request
 app.delete('/api/notes/:id', (req, res) => {
-
-});
-//Receive query parameter to locate note to delete. In order to delete a note, read all the notes from db.json
-//remove the note with the given id and rewrite notes to db.json
-app.delete('/api/notes:title', (req, res) => {
-    let selected = req.params.title;
+    let selected = req.params.id;
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if(err) {
             throw err
         }
 
         let json = JSON.parse(data);
-        const toDelete = json.find(note => note.title === selected);
+        const toDelete = json.find(note => {
+            console.log(note.id);
+            console.log(selected);
+            return note.id == selected
+        });
         if(!toDelete) {
-            res.status(404).send('Note with given title could not be located');
+            res.status(404).send('Note with given ID could not be located');
         };
         const index = json.indexOf(toDelete);
         json.splice(index, 1);
-    });   
-});
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on PORT ${PORT}`);
+        fs.writeFile('./db/db.json', JSON.stringify(json), (err) => {
+            if (err) {
+                throw err
+            }
+            res.json({ ok: true });
+        });
+    });  
 });
